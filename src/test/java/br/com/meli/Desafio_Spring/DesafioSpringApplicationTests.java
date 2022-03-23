dbc563527654b71763772a2fe58c2b4296169977
julio cesar gama <juliocesargama@protonmail.com>
2022-03-23 15:10:40 -0300

    first commit

---
+++ src/test/java/br/com/meli/Desafio_Spring/DesafioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.Desafio_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
