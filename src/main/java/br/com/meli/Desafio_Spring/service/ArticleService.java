e67de59d0cf9a40457640e89438ccd8a368c9ea0
Hugo Freire <ahugo.freire@gmail.com>
2022-03-24 10:33:18 -0300

    add save method

--- src/main/java/br/com/meli/Desafio_Spring/service/ArticleService.java
+++ src/main/java/br/com/meli/Desafio_Spring/service/ArticleService.java
@@ -1,4 +1,29 @@
 package br.com.meli.Desafio_Spring.service;
 
+import br.com.meli.Desafio_Spring.entity.Article;
+import br.com.meli.Desafio_Spring.repository.ArticleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public Article save(Article article) {
+        return articleRepository.save(article);
+    }
+
+    public List<Article> save(List<Article> articles) {
+        List<Article> articleCreated  = new ArrayList<Article>();
+
+        articles.forEach(article -> {
+            articleCreated.add(articleRepository.save(article));
+        });
+
+        return articleCreated;
+    }
 }
