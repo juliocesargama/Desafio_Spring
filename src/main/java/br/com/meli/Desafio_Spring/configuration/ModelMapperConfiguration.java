bc27fb3dd1b05648835f087586293e2c29a865e4
Hugo Freire <ahugo.freire@gmail.com>
2022-03-24 10:28:30 -0300

    add ModelMapperConfiguration

--- src/main/java/br/com/meli/Desafio_Spring/configuration/ModelMapperConfiguration.java
+++ src/main/java/br/com/meli/Desafio_Spring/configuration/ModelMapperConfiguration.java
@@ -1,2 +1,14 @@
-package br.com.meli.Desafio_Spring.configuration;public class ModelMapperConfiguration {
+package br.com.meli.Desafio_Spring.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfiguration {
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
 }
