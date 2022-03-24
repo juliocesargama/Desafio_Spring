2a238613877ea75420fc8c10823b50fc752f284f
Ana Preis <ana.preis@meradolivre.com>
2022-03-24 10:00:47 -0300

    articleDTO convert creation

--- src/main/java/br/com/meli/Desafio_Spring/dto/ArticleDTO.java
+++ src/main/java/br/com/meli/Desafio_Spring/dto/ArticleDTO.java
@@ -1,5 +1,6 @@
 package br.com.meli.Desafio_Spring.dto;
 
+import br.com.meli.Desafio_Spring.entity.Article;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,11 @@ public class ArticleDTO {
     private long productId;
     private String name;
     private Integer quantity;
+
+    public ArticleDTO convert(Article article){
+        this.productId = article.getProductId();
+        this.name = article.getName();
+        this.quantity = article.getQuantity();
+        return this;
+    }
 }
