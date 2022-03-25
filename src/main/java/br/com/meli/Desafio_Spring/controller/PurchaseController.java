55a3d5942e0724cb1a000a8b6d3eb2dab861ba8c
Ana Preis <ana.preis@mercadolivre.com>
2022-03-25 12:26:27 -0300

    creating purchase id list in client

--- src/main/java/br/com/meli/Desafio_Spring/controller/PurchaseController.java
+++ src/main/java/br/com/meli/Desafio_Spring/controller/PurchaseController.java
@@ -4,6 +4,8 @@ import br.com.meli.Desafio_Spring.dto.PurchaseArticleDTO;
 import br.com.meli.Desafio_Spring.dto.RequestPurchaseDTO;
 import br.com.meli.Desafio_Spring.entity.Article;
 import br.com.meli.Desafio_Spring.entity.Purchase;
+import br.com.meli.Desafio_Spring.entity.ShoppingCart;
+import br.com.meli.Desafio_Spring.repository.ClientRepository;
 import br.com.meli.Desafio_Spring.service.PurchaseService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -12,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,17 +27,21 @@ public class PurchaseController {
     @Autowired
     PurchaseService purchaseService;
 
+    @Autowired
+    ClientRepository clientRepository;
+
     private final ModelMapper modelMapper;
 
     @PostMapping("/api/v1/purchase-request")
-    public ResponseEntity<Purchase> purchaseArticles(@RequestBody RequestPurchaseDTO purchaseArticleDTOS) {
+    public ResponseEntity<Purchase> purchaseArticles(@RequestBody RequestPurchaseDTO purchaseArticleDTOS,
+                                                     @RequestParam long id) {
 
         List<PurchaseArticleDTO> PurchaseArticleDTO = purchaseArticleDTOS.getArticlesPurchaseRequest().stream()
                 .map(purchase -> modelMapper.map(purchase, PurchaseArticleDTO.class))
                 .collect(Collectors.toList());
 
-        System.out.println("endpoint");
         Purchase purchase = purchaseService.save(PurchaseArticleDTO);
+        clientRepository.addPurchaseIdToList(purchase, id);
 
         return new ResponseEntity<Purchase>(purchase, HttpStatus.CREATED);
     }
