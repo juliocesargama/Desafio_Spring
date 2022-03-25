5abd085cfba2c2f9c91d17f6dee9dba843ab2657
Hugo Freire <ahugo.freire@gmail.com>
2022-03-25 17:54:53 -0300

    add totalDTO

--- src/main/java/br/com/meli/Desafio_Spring/dto/TotalDTO.java
+++ src/main/java/br/com/meli/Desafio_Spring/dto/TotalDTO.java
@@ -1,2 +1,12 @@
-package br.com.meli.Desafio_Spring.dto;public class TotalDTO {
+package br.com.meli.Desafio_Spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class TotalDTO {
+    BigDecimal totalPurchases;
 }
