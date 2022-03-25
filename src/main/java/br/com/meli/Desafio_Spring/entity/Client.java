be2ae2a7e99398c5aa857c36db90a026b93f7752
Ana Preis <ana.preis@mercadolivre.com>
2022-03-25 12:26:27 -0300

    creating purchase id list in client

--- src/main/java/br/com/meli/Desafio_Spring/entity/Client.java
+++ src/main/java/br/com/meli/Desafio_Spring/entity/Client.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,10 +15,12 @@ public class Client {
     private String name;
     private String email;
     private UF uf;
+    private List<Long> idList;
 
     public Client(String name, String email, UF uf) {
         this.name = name;
         this.email = email;
         this.uf = uf;
+        this.idList = new ArrayList<>();
     }
 }
--- src/main/java/br/com/meli/Desafio_Spring/repository/ClientRepository.java
+++ src/main/java/br/com/meli/Desafio_Spring/repository/ClientRepository.java
@@ -1,7 +1,9 @@
 package br.com.meli.Desafio_Spring.repository;
 
 import br.com.meli.Desafio_Spring.entity.Client;
+import br.com.meli.Desafio_Spring.entity.Purchase;
 import br.com.meli.Desafio_Spring.entity.UF;
+import br.com.meli.Desafio_Spring.exception.EntityNotFoundException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
@@ -17,6 +19,7 @@ public class ClientRepository {
 
     private final String FILE_PATH="src/main/resources/clients.json";
     private final ObjectMapper mapper = new ObjectMapper();
+    private List<Client> clients = getAll();
 
     public List<Client> getAll() {
         try {
@@ -47,5 +50,15 @@ public class ClientRepository {
         List<Client> clientStream = allClient.stream().filter(client ->  client.getUf() == uf).collect(Collectors.toCollection(ArrayList::new));
 
         return clientStream;
+    }
+
+    public void addPurchaseIdToList(Purchase purchase, long idCliente){
+        Client client = clients.stream()
+                        .filter(a -> a.getId() == idCliente)
+                        .findFirst().orElseThrow(() -> new EntityNotFoundException("Id not found " + idCliente));
+        if (client.getIdList() == null){
+            client.setIdList(new ArrayList<>());
+        }
+        client.getIdList().add((Long) purchase.getId());
     }
 }
