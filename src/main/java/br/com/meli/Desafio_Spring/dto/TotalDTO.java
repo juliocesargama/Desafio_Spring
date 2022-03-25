a3fcb6c510e24ba234ed47f19576725dafe37801
Hugo Freire <ahugo.freire@gmail.com>
2022-03-25 18:04:51 -0300

    refactor totalDTO closes #54

--- src/main/java/br/com/meli/Desafio_Spring/dto/TotalDTO.java
+++ src/main/java/br/com/meli/Desafio_Spring/dto/TotalDTO.java
@@ -2,11 +2,13 @@ package br.com.meli.Desafio_Spring.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TotalDTO {
     BigDecimal totalPurchases;
 }
