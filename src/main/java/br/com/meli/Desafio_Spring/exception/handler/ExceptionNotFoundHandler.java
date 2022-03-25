a414e598d0205836b27732d4d4c63b637ccb142d
Hugo Freire <ahugo.freire@gmail.com>
2022-03-25 11:30:23 -0300

    add path in ExceptionArticleDTO closes #26

--- src/main/java/br/com/meli/Desafio_Spring/exception/handler/ExceptionNotFoundHandler.java
+++ src/main/java/br/com/meli/Desafio_Spring/exception/handler/ExceptionNotFoundHandler.java
@@ -7,19 +7,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ExceptionNotFoundHandler {
     @ExceptionHandler(value = {EntityNotFoundException.class})
-    protected ResponseEntity<Object> handleNotFoundArticleException(EntityNotFoundException exception) {
+    protected ResponseEntity<Object> handleNotFoundArticleException(EntityNotFoundException exception, HttpServletRequest request) {
         ExceptionArticleDTO exceptionArticleDTO = ExceptionArticleDTO.builder()
                 .timestamp(LocalDateTime.now())
                 .title("Resource not found")
                 .statusCode(HttpStatus.NOT_FOUND.value())
                 .message(exception.getMessage())
+                .path(request.getRequestURI())
                 .build();
 
         return new ResponseEntity<>(exceptionArticleDTO, HttpStatus.NOT_FOUND);
     }
+
 }
